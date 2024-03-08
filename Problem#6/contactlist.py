b72ebcfdc03d4edefd26732483d25a349670f22f
unknown <isaacneal5@gmail.com>
2024-03-08 14:37:01 -0600

    HW

--- Problem#6/contactlist.py
+++ Problem#6/contactlist.py
@@ -1,20 +1,21 @@
 def process_user_contacts(user_input):
-
+    split = user_input
+    print(split)
 
     # Put word pairs into a dictionary
     # Get contact name from input, output contact's phone number
     contact_name = input("Enter the contact name: ")
     for i in range(len(user_input)):
-        if(user_input[i] == contact_name):
+        if(split[i] == contact_name):
             break
-    print(user_input[i][(len(contact_name)+1)::])
+
+    print(split[i+1])
 
     
    
 if __name__ == '__main__':
     # Get input for word pairs
     user_input = input("Enter word pairs (name, phone number): ")
-    split = user_input.split()
 
     # Call the function to process user contacts
-    process_user_contacts(split)
+    process_user_contacts(user_input.split())
