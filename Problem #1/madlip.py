4b88ec869a7957d4aff9c56cc9e5fd43477266b0
unknown <isaacneal5@gmail.com>
2024-03-08 12:01:51 -0600

    HW

--- Problem #1/madlip.py
+++ Problem #1/madlip.py
@@ -3,7 +3,7 @@ def food_input():
     tokens = user_input.split()
     #type you while  loop here
     while(user_input != "quit"):
-        print("eating " + tokens[1] + " " + tokens[0]+ " a day keeps you happy and healthy.")
+        print("Eating " + tokens[1] + " " + tokens[0]+ " a day keeps you happy and healthy.")
         user_input = input()
         tokens = user_input.split()
         if(tokens[0] == "quit"):
